3f6aed01c5df70222e272990f21c97d2698415b6
acidlord <acidlord_34@abv.bg>
2024-09-14 23:37:29 +0300

    Source Added

---
+++ GuessANumber/Program.cs
@@ -0,0 +1,25 @@
+﻿Random randomNumber = new Random();
+int computerNumber = randomNumber.Next(1,101);
+Console.WriteLine($" /tGuess A Number");
+while (true)
+{
+    Console.Write("Guess a number (1-100): ");
+    bool isValid = int.TryParse(Console.ReadLine(), out int playerNumber);
+    if (isValid)
+    {
+        if (playerNumber > computerNumber)
+        {
+            Console.WriteLine("Too High!");
+        }
+        else if (playerNumber < computerNumber)
+        {
+            Console.WriteLine("Too Low!");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+            break;
+        }
+    }
+    else { Console.WriteLine("Invalid input! Try again."); }
+}
